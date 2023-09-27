9febeb1a7cf92bbd1fe435ebead9698d59e7dc7d
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-09-27 15:43:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Mash'
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+const capitalized = currentUser.toUpperCase()
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + capitalized + '!' 
+const firstInitial = currentUser[0]
+const shortGreeting = 'Welcome, ' + firstInitial + '!'
+
